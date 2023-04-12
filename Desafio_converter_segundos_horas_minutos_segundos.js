888be0153c81c8b3d8402b4774192b83ba750d90
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-04-11 22:06:04 -0300

    colocando comentários

--- Desafio_converter_segundos_horas_minutos_segundos.js
+++ Desafio_converter_segundos_horas_minutos_segundos.js
@@ -1,11 +1,11 @@
 
 
-const seconds = 108000;
+const seconds = 108391;
 
 function converty(seconds) {
     let h = Math.floor(seconds / 3600)
-    let m = Math.floor((seconds % 3600) / 60)
-    let s = seconds % 60
+    let m = Math.floor((seconds % 3600) / 60) // Pegar o resto da divisão
+    let s = seconds % 60 // Pegar o resto da divisão
 
     return {
         h,
